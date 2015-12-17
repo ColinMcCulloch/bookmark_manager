8889a96c32cd2ce7301d12064e7f2287e1bf26c6
Colin McCulloch <mccolane@gmail.com>
2015-12-17 15:55:51 +0000

    pt16

--- app/app.rb
+++ app/app.rb
@@ -20,6 +20,14 @@ class BookmarkManager < Sinatra::Base
    redirect to('/links')
   end
 
+  get '/tags/:name' do
+    tag = Tag.first(name: params[:name])
+    @links = tag ? tag.links : []
+    erb :'links/index'
+  end
+
+
+
   get '/links/new' do
     erb(:'links/new')
   end
