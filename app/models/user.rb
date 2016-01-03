a687df26d1182a2772daa41ae76a7fe2008e424d
Colin McCulloch <mccolane@gmail.com>
2016-01-03 11:21:01 +0000

    pt19

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,18 @@ require 'bcrypt' # make sure 'bcrypt' is in your Gemfile
 class User
   include DataMapper::Resource
 
+  attr_reader :password
+  attr_accessor :password_confirmation
+
+  # validates_confirmation_of is a DataMapper method
+   # provided especially for validating confirmation passwords!
+   # The model will not save unless both password
+   # and password_confirmation are the same
+   # read more about it in the documentation
+   # http://datamapper.org/docs/validations.html
+  validates_confirmation_of :password
+
+
   property  :id,              Serial
   property  :email,           String
   property  :password_digest, Text
@@ -21,6 +33,7 @@ class User
   # has both the password hash and the salt. We save it to the
   # database instead of the plain password for security reasons.
   def password=(password)
+    @password = password
     self.password_digest = BCrypt::Password.create(password)
   end
 
